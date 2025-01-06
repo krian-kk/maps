1ef7a8cf8cafa836ccd39c78ba7893b054d5abd6
krian <krishnachouhan414@gmail.com>
2025-01-06 11:49:19 +0530

    url changed

--- lib/features/product/repository/product_repository.dart
+++ lib/features/product/repository/product_repository.dart
@@ -8,7 +8,7 @@ class ProductRepository {
 
   Future<List<Product>> fetchProducts() async {
     final response = await dio.get(
-      'https://h2gvbfqo6smwzaby6x7jinzujm0otrjt.lambda-url.eu-west-2.on.aws/',
+      'https://prcddzpsk5p33eazh5ykzbj4ra0xwyhk.lambda-url.eu-west-2.on.aws/',
     );
     try {
       if (response.statusCode == 200) {
